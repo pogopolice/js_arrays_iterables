93144aad8d73d196b518bcca921df2652371c95d
pogopolice <pogopolice@gmail.com>
2022-04-07 08:55:01 -0400

    make some examples of indexOf and lastIndex

--- app.js
+++ app.js
@@ -50,10 +50,16 @@
 
 
 
-const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
 // const storedResults = testResults.slice(2);
 const storedResults = testResults.concat([3.99, 2]);
 
 testResults.push(5.91);
 
 console.log(storedResults, testResults);
+
+console.log(testResults.indexOf(1.5));
+console.log(testResults.lastIndexOf(1.5));
+
+const personData = [{name: 'Max'}, {name: 'Manuel'}];
+console.log(personData.indexOf({name: 'Manuel'}));  // returns -1 for not found
