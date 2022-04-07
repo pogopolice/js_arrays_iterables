8160e15847abacb861a2a03b09426916a029aa54
pogopolice <pogopolice@gmail.com>
2022-04-07 16:56:12 -0400

    use map in an array

--- app.js
+++ app.js
@@ -83,17 +83,29 @@
 
 
 
-const prices = [10.99, 5.99, 3.99, 6.59];
-const tax = 0.19;
-const taxAdjustedPrices = [];
-
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+// const taxAdjustedPrices = [];
+// 
 // for (const price of prices) {
   // taxAdjustedPrices.push(price * (1 + tax))
 // }
+// 
+// prices.forEach((price, idx, prices) => {
+  // const priceObj = {index: idx, taxAdjustedPrice: price * (1 + tax) }
+  // taxAdjustedPrices.push(priceObj);
+// });
+// 
+// console.log(taxAdjustedPrices);
 
-prices.forEach((price, idx, prices) => {
+
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
   const priceObj = {index: idx, taxAdjustedPrice: price * (1 + tax) }
-  taxAdjustedPrices.push(priceObj);
+  return priceObj;
 });
 
 console.log(taxAdjustedPrices);
