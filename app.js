58637af56840b0c19e0dcf26d561ac650d5258dd
pogopolice <pogopolice@gmail.com>
2022-04-14 20:54:38 -0400

    add some exercises for the join and split method

--- app.js
+++ app.js
@@ -141,3 +141,13 @@ const sum = prices.reduce((prevValue, curValue) => {
 }, 0);
 
 console.log(sum);
+
+const data = 'new york;10.99;2000';
+
+const transformedData = data.split(';');
+transformedData[1] = +transformedData[1];
+console.log(transformedData);
+
+const nameFragments = ['Mister', 'Person'];
+const title = nameFragments.join(' ');
+console.log(title);
