a833e35f7a5b85ef6e041d21a5b4c0364d222a7d
pogopolice <pogopolice@gmail.com>
2022-04-19 21:23:59 -0400

    add some exercises for using weaksets and weakmaps

--- setsmaps.js
+++ setsmaps.js
@@ -11,26 +11,42 @@
 //   console.log(entry);
 // }
 
-const person1 = {name: 'Mister'};
-const person2 = {name: 'Person'};
+// const person1 = {name: 'Mister'};
+// const person2 = {name: 'Person'};
 
-const personData = new Map([[person1, [{date: 'yesterday', price: 10}]]]);
+// const personData = new Map([[person1, [{date: 'yesterday', price: 10}]]]);
 
-personData.set(person2, [{date: 'two weeks ago', price: 100}]);
+// personData.set(person2, [{date: 'two weeks ago', price: 100}]);
 
+// console.log(personData);
+// console.log(personData.get(person1));
+
+// for (const [key, value] of personData.entries()) {
+//   console.log(key, value)
+// }
+
+// for (const key of personData.keys()) {
+//   console.log(key);
+// }
+
+// for (const value of personData.values()) {
+//   console.log(value);
+// }
+
+// console.log(personData.size)
+
+
+let person = {name: 'Mister'};
+const persons = new WeakSet();
+persons.add(person);
+
+// ... some operation
+// person = null;
+
+console.log(persons);
+
+const personData = new WeakMap();
+personData.set(person, 'Extra Info!');
+
+person = null;
 console.log(personData);
-console.log(personData.get(person1));
-
-for (const [key, value] of personData.entries()) {
-  console.log(key, value)
-}
-
-for (const key of personData.keys()) {
-  console.log(key);
-}
-
-for (const value of personData.values()) {
-  console.log(value);
-}
-
-console.log(personData.size)
